004bcd94f20bdb814f347532378b466d18b45c14
Lukas Jukna <jukna.lukas23@gmail.com>
2021-04-07 20:42:09 +0300

    initial backend project

---
+++ Backend/Videoteka.API/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Videoteka.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public string GetHealth()
+        {
+            return "healthy";
+        }
+    }
+}
