2a3cfaca21a8cbd25e08cb03e071112ab75ca5da
idioidea <idio.idea@gmail.com>
2021-04-15 13:20:29 +0300

    ONLY-44 Layout component

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Button, MuiThemeProvider } from "@material-ui/core";
 import VideoContainer from "./Video";
 import theme from "./theme";
+import Layout from './components/Layout/Layout'
 
 function App() {
     const videoPlayerOptions = (src, type) => ({
@@ -20,8 +21,11 @@ function App() {
     return (
         <MuiThemeProvider theme={theme}>
             <div className="App">
+                <Layout/>
+                {/*
                 <VideoContainer playerOptions={videoPlayerOptions('videoteka-video.mp4', 'video/mp4')}/>
                 <Button variant="contained" color="primary">Share</Button>
+                */}
             </div>
         </MuiThemeProvider>
     );
---
+++ Frontend/src/components/Layout/Layout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../Layout/Layout.css'
+
+function Layout () {
+
+    return(        
+        <div className="Layout">
+            <div className="Layout-Sidebar">
+                replace div with Sidebar component
+            </div>
+            <div className="Layout-Header" >
+                replace div with Header component
+            </div>
+            <div className="Layout-Middle" >
+                replace div with Middle component
+            </div>            
+        </div>        
+    )
+}
+
+export default Layout;
