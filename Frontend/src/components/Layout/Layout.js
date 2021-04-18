896b8be0d8296f137686334c9caa3c505dd8195f
LukasElenbergas <lukas.elenbergas@gmail.com>
2021-04-18 18:27:34 +0300

    First iteration of Header Content

--- Frontend/src/components/Layout/Layout.js
+++ Frontend/src/components/Layout/Layout.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Layout/Layout.scss'
+import { Toolbar } from '@material-ui/core';
+import SearchBar from "material-ui-search-bar";
 
-function Layout () {
-    return(        
+function Layout() {
+
+    const [folderName, setFolderName] = useState("Folder");
+
+    return (
         <div className="Layout">
+
             <div className="Layout__Sidebar">
                 replace div with Sidebar component
             </div>
+
             <div className="Layout__Content" >
+
                 <div className="Layout__Header" >
-                    replace div with Header component
+                    <div className="HeaderContent">
+                        <div>
+                            
+                        </div>
+                        <Toolbar id="ToolbarAdjustment">
+                            <h2> {folderName} </h2>
+                            <div className="FolderName">
+                                <button className="MenuButton"> Add new video </button>
+                                <button className="MenuButton InfoButton"> i </button>
+                            </div>
+                        </Toolbar>
+                    </div>
                 </div>
+
                 <div className="Layout__Middle" >
                     replace div with Middle component
-                </div>  
+                </div>
+
             </div>
-                     
-        </div>        
+        </div>
     )
 }
 
