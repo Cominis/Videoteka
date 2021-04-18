0cf066cb7dec206b9d5f3d12e9d9d3a20ea027ad
LukasElenbergas <lukas.elenbergas@gmail.com>
2021-04-18 18:53:47 +0300

    Adding the Search bar to the Header Component

--- Frontend/src/components/Layout/Layout.js
+++ Frontend/src/components/Layout/Layout.js
@@ -3,9 +3,17 @@ import '../Layout/Layout.scss'
 import { Toolbar } from '@material-ui/core';
 import SearchBar from "material-ui-search-bar";
 
+const backendURL = "..."
+
+function doSomethingWith(searchTextValue) {
+    const urlToCall = backendURL + "/search/" + searchTextValue;
+    //actually call the url
+}
+
 function Layout() {
 
     const [folderName, setFolderName] = useState("Folder");
+    const [searchText, setSearchText] = useState("");
 
     return (
         <div className="Layout">
@@ -19,7 +27,11 @@ function Layout() {
                 <div className="Layout__Header" >
                     <div className="HeaderContent">
                         <div>
-                            
+                            <SearchBar
+                                //value={this.state.value}
+                                onChange={(newValue) => setSearchText({ searchText: newValue })}
+                                onRequestSearch={() => doSomethingWith(searchText)}
+                            />
                         </div>
                         <Toolbar id="ToolbarAdjustment">
                             <h2> {folderName} </h2>
