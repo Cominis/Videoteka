c25cf7541c01aefac3242798c4483d0df0b1d0fa
Daumantas Šimkus <daumantas.simkus@gmail.com>
2021-04-25 17:22:13 +0300

    Initial drawer

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,30 +1,42 @@
 import { Button, MuiThemeProvider } from "@material-ui/core";
 import VideoContainer from "./Video";
+import AccordionTest from "./Drawer/ContentAccordion";
+import Drawer from "./Drawer/Drawer";
 import theme from "./theme";
 
 function App() {
-    const videoPlayerOptions = (src, type) => ({
-        autoplay: false,
-        playbackRates: [0.25, 0.5, 1, 1.5, 2],
-        width: 768,
-        height: 432,
-        controls: true,
-        sources: [
-            {
-                src,
-                type,
-            },
-        ],
-    });
+  const videoPlayerOptions = (src, type) => ({
+    autoplay: false,
+    playbackRates: [0.25, 0.5, 1, 1.5, 2],
+    width: 768,
+    height: 432,
+    controls: true,
+    sources: [
+      {
+        src,
+        type,
+      },
+    ],
+  });
 
-    return (
-        <MuiThemeProvider theme={theme}>
-            <div className="App">
-                <VideoContainer playerOptions={videoPlayerOptions('videoteka-video.mp4', 'video/mp4')}/>
-                <Button variant="contained" color="primary">Share</Button>
-            </div>
-        </MuiThemeProvider>
-    );
+  return (
+    <MuiThemeProvider theme={theme}>
+      <Drawer>
+        <div className="App">
+          <AccordionTest />
+          <VideoContainer
+            playerOptions={videoPlayerOptions(
+              "videoteka-video.mp4",
+              "video/mp4"
+            )}
+          />
+          <Button variant="contained" color="primary">
+            Share
+          </Button>
+        </div>
+      </Drawer>
+    </MuiThemeProvider>
+  );
 }
 
 export default App;
