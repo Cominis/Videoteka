e3eddb8551ed05e4d270c6fcfc02f70ca31e341b
Lukas Jukna <jukna.lukas23@gmail.com>
2021-05-25 20:28:46 +0300

    add default user

---
+++ Backend/Videoteka.API/Migrations/20210525171945_AddDefaultUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Videoteka.API.Migrations
+{
+    public partial class AddDefaultUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Users DEFAULT VALUES;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
